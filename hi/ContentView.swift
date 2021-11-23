02ad6384d6bd17d84b3ea7d3fa6ddd0d5a1d951e
ToPu <topu@programmer.net>
2021-11-24 01:46:09 +0700

    say hi
    ;
    /Users/topu/Documents/iosXcodeFiles/hi/hi.xcodeproj /Users/topu/Documents/iosXcodeFiles/hi/hi /Users/topu/Documents/iosXcodeFiles/hi/hiTests /Users/topu/Documents/iosXcodeFiles/hi/hiUITests /Users/topu/Documents/iosXcodeFiles/hi/hi.xcodeproj /Users/topu/Documents/iosXcodeFiles/hi/hi /Users/topu/Documents/iosXcodeFiles/hi/hiTests /Users/topu/Documents/iosXcodeFiles/hi/hiUITests

---
+++ hi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hi
+//
+//  Created by ToPu on 24/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi ToPu, how do you do!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
